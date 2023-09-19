9f127f6a9ea0d8300fdb7ea9c9b5a7635494c877
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-09-19 23:58:33 +0530

    Update sqrtxmoreoresicion.cpp

--- sqrtxmoreoresicion.cpp
+++ sqrtxmoreoresicion.cpp
@@ -42,3 +42,45 @@ int main(){
     cout <<"answer is" << moreprecision (n,3,tempsol)<<endl;
     return 0;
 }
+
+
+
+__________________________________________________________________________________________________________________________
+#include<iostream>
+using namespace std;
+int sqrtinterger(int n){
+ int s=0;
+int e=n;
+long long int mid=s+(e-s)/2;
+while(e<s){
+int square = mid*mid;
+if (square==n)
+return mid;
+if(square<n}{
+ ans=mid;
+s=mid+1;
+}
+ else{
+  e=mid-1;
+ }
+ mid=s+(e-s)/2;
+}
+return ans;
+}
+int moreprecision(int n, int presicion, int tempsol)
+double factor=1;
+double ans= tempsol;
+for(int i=0;i<presicion;i++){
+ factor=facto/10;
+ for(double j=ans; j*j<n; j=j+factor){
+  ans=j;
+}
+}
+int main(){
+ int n;
+cout << "enter the value" << endl;
+cin>>n;
+int tempsol=sqrtiterger(n);
+cout<<"answer is"<< moreprecision  (n,3,tempsol)<< endl;
+return 0;
+}
